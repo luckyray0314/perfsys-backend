24a87a0697041f68d99072ce6e0705cb13e9b099
Michael Moore <zhangyunpei816@gmail.com>
2023-12-07 17:40:26 -0800

    vercel updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoURI: "mongodb://127.0.0.1:27017/manage_devgroups",
+  // mongoURI: "mongodb://127.0.0.1:27017/manage_devgroups",
+  mongoURI: "mongodb+srv://luckyangel314:OAql28IVemHJQUYU@cluster0.hrff1nk.mongodb.net/manage_devgroups",
   jwtSecret: "kut10developers",
   githubToken: "<yoursecrectaccesstoken>",
 };
